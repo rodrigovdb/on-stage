731b854d52293f08e4c7c24769f17af753a44fac
Rodrigo Otavio van den Berg Maia <rodrigo.maia@lendingloop.ca>
2022-01-14 21:27:40 -0500

    messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  NOTICE = {
+    create: 'created successfully',
+    update: 'updated successfully',
+    delete: 'deleted successfully',
+    error: 'Something went wrong'
+  }.freeze
+
+  def notice_stream(domain:, message:, status:)
+    if message == :error
+      message = NOTICE[message]
+    else
+      message = "#{domain.capitalize} #{NOTICE[message]}"
+    end
+
+    turbo_stream.replace 'notice', partial: 'notice', locals: { notice: message, status: status }
+  end
 end
