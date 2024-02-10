85441edb38ed5d0b331e9634d9f157b80b073cf4
Rodrigo Otavio van den Berg Maia <rodrigo.maia@wekowtraining.ca>
2024-02-09 22:19:51 -0500

    track duration

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,14 @@ class Song < ApplicationRecord
   belongs_to :band
 
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: { scope: :band_id }
+
+  def track_duration=(string)
+    self.duration = TrackDuration.new(string).seconds
+  end
+
+  def track_duration
+    return unless duration
+
+    TrackDuration.new(duration).to_s
+  end
 end
